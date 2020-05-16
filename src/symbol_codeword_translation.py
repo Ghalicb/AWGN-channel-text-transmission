12e0e816f9ca95574b98ac5fb02e12d19e50f24a
Ghali C <ghalicb@gmail.com>
2020-05-16 13:30:04 +0200

    Add the skeleton of a file responsible of the translation symbol <=> codeword

---
+++ src/symbol_codeword_translation.py
@@ -0,0 +1,33 @@
+
+
+# ord('a') -> 97  (le numéro ascii de 'a')
+# chr(97)  -> 'a' (le charactère associé au 98ème élément de la table ascii)
+
+def symbol_to_codeword():
+    """
+
+
+    Parameters
+    ----------
+
+
+    Returns
+    -------
+
+    """
+
+
+
+
+def codeword_to_symbol():
+    """
+
+
+    Parameters
+    ----------
+
+
+    Returns
+    -------
+
+    """
