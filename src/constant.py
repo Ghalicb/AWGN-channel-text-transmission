818f70c626e1c3af127ed62c88cbe3466fc863df
Ghali C <ghalicb@gmail.com>
2020-05-21 23:38:51 +0200

    Add last needed constants for message and guess file path

--- src/constant.py
+++ src/constant.py
@@ -8,8 +8,14 @@ SIZE_H = 128
 "Number of time we repeat a codeword to improve the chance of recovering it perfectly"
 REPETITIONS = 1
 
+"File path for the original message"
+MESSAGE_FILE = '../res/message.txt'
+
 "File path for the transmitter input"
 INPUT_FILE = '../res/input.txt'
 
 "File path for the receiver output"
 OUTPUT_FILE = '../res/output.txt'
+
+"File path for the guess"
+GUESS_FILE = '../res/guess.txt'
