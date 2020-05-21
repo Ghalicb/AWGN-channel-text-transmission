75729e9c4bb50fa782d8a76dee6b00f1db2cd97b
Ghali C <ghalicb@gmail.com>
2020-05-21 14:59:00 +0200

    Add a file to store all the constant

---
+++ src/constant.py
@@ -0,0 +1,9 @@
+"""
+File containing all the necessary constant for this project
+"""
+
+"Size of the Hadamard matrix, allowing an alphabet of 2 * SIZE_H symbols for the message"
+SIZE_H = 128
+
+"Number of time we repeat a codeword to improve the chance of recovering it perfectly"
+REPETITIONS = 1
