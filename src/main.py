cb3fe6739ce1cf966b9675c23997bb8e66924446
Ghali C <ghalicb@gmail.com>
2020-05-21 23:41:33 +0200

    Add a main that gather all the steps to transmit a message

---
+++ src/main.py
@@ -0,0 +1,23 @@
+import os
+
+from src.encode import encode
+from src.decode import decode
+from src.parsing import read_message, write_guess
+from src.constant import MESSAGE_FILE, INPUT_FILE, OUTPUT_FILE, GUESS_FILE
+
+
+def main():
+    """ Execute a full transmission of a text file through an Additive Gaussian Noise channel. """
+
+    symbols = read_message(MESSAGE_FILE)
+    encode(symbols)
+
+    os.system('python3 ../client/client.py --input_file=' + INPUT_FILE +
+              ' --output_file=' + OUTPUT_FILE + ' --srv_hostname=iscsrv72.epfl.ch --srv_port=80')
+
+    recovered_symbols = decode()
+    write_guess(recovered_symbols, GUESS_FILE)
+
+
+if __name__ == "__main__":
+    main()
